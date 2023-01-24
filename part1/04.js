d24a5b9d02e02dc18b42b17f3d464f97d6868cdb
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2023-01-24 10:59:28 +0530

    Update 04.js

--- part1/04.js
+++ part1/04.js
@@ -5,5 +5,8 @@ let firstName = "harshit";
 firstName = "Mohit";
 console.log(firstName);
 
+//var firstName="hello";
+//var firstName = "kello"; here it is valid but in let it is not means we cant define two same name variable using let
+
 
 // block scope vs funtion scope (covered later in this video)
